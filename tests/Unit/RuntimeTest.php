45fa7378dfe6c773344bdb708130df6c0ebb6552
Vasili Kamrakou <diolektor@gmail.com>
2026-04-25 16:16:44 +0400

    test: use NO_HANDLER to skip global handler registration in Runtime tests

--- tests/Unit/RuntimeTest.php
+++ tests/Unit/RuntimeTest.php
@@ -20,6 +20,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 final class RuntimeTest extends TestCase
 {
+    /** Skip SymfonyRuntime's global error/exception handler registration; tests don't exercise it. */
+    private const array NO_HANDLER = ['error_handler' => false];
+
     protected function setUp(): void
     {
         OxPHPHarness::reset();
@@ -27,35 +30,35 @@ final class RuntimeTest extends TestCase
 
     public function test_dispatches_psr15_handler_to_psr15_runner(): void
     {
-        $runtime = new Runtime();
+        $runtime = new Runtime(self::NO_HANDLER);
         $runner = $runtime->getRunner(FakePsr15Handler::echoingPath());
         self::assertInstanceOf(Psr15Runner::class, $runner);
     }
 
     public function test_dispatches_laravel_kernel_to_laravel_runner(): void
     {
-        $runtime = new Runtime();
+        $runtime = new Runtime(self::NO_HANDLER);
         $runner = $runtime->getRunner(FakeLaravelKernel::echoingPath());
         self::assertInstanceOf(LaravelRunner::class, $runner);
     }
 
     public function test_dispatches_symfony_kernel_to_http_kernel_runner(): void
     {
-        $runtime = new Runtime();
+        $runtime = new Runtime(self::NO_HANDLER);
         $runner = $runtime->getRunner(FakeSymfonyKernel::echoingPath());
         self::assertInstanceOf(HttpKernelRunner::class, $runner);
     }
 
     public function test_dispatches_http_foundation_response(): void
     {
-        $runtime = new Runtime();
+        $runtime = new Runtime(self::NO_HANDLER);
         $runner = $runtime->getRunner(new Response('x'));
         self::assertInstanceOf(HttpFoundationResponseRunner::class, $runner);
     }
 
     public function test_dispatches_psr7_response(): void
     {
-        $runtime = new Runtime();
+        $runtime = new Runtime(self::NO_HANDLER);
         $response = new Psr17Factory()->createResponse(204);
         $runner = $runtime->getRunner($response);
         self::assertInstanceOf(Psr7ResponseRunner::class, $runner);
@@ -63,7 +66,7 @@ final class RuntimeTest extends TestCase
 
     public function test_unsupported_type_throws_logic_exception(): void
     {
-        $runtime = new Runtime();
+        $runtime = new Runtime(self::NO_HANDLER);
 
         $this->expectException(\LogicException::class);
         $this->expectExceptionMessage('OxPHP Runtime does not support');
@@ -74,7 +77,7 @@ final class RuntimeTest extends TestCase
     public function test_resetters_option_is_forwarded_to_runners(): void
     {
         $called = 0;
-        $runtime = new Runtime(['resetters' => [static function () use (&$called): void {
+        $runtime = new Runtime(self::NO_HANDLER + ['resetters' => [static function () use (&$called): void {
             $called++;
         }]]);
 
